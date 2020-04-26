05e95569a6cda2ee2d45c089e8968c8adaee588b
John Cook <johngncook@gmail.com>
2020-04-26 08:21:37 -0700

    Added environment variables to secret key and debug. Added environment variables to heroku.

--- mango_tree/settings.py
+++ mango_tree/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ukhlxr&u1$lkg*hj0l5)pdw@q204)vdcg#%=12*0d6#g@hly&$'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG_VALUE')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mangoblog.herokuapp.com']
 
 #'mangoblog.herokuapp.com'
 
